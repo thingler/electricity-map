6cac3313c2196f72654bfc9194a6c9980577706e
Jarl Törnroos <jarl.tornroos@gmail.com>
2023-02-03 15:00:19 +0200

    react helmet for dynamic meta tags

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 npm run build
-aws s3 cp build/ s3://static.thingler.io/ --recursive --exclude 'index.html' --exclude '.DS_Store' --cache-control 'max-age=2678400'
-aws s3 cp build/index.html s3://static.thingler.io/ --cache-control 'max-age=600'
+aws s3 cp build/ s3://static.thingler.io/ --recursive --cache-control 'max-age=2678400' \
+    --exclude '*.html' \
+    --exclude 'map.png' \
+    --exclude '.DS_Store'
+aws s3 cp build/map.png s3://static.thingler.io/ --cache-control 'max-age=600'
+aws s3 cp build/*.html s3://static.thingler.io/ --cache-control 'max-age=600'
--- frontend/src/components/Page/Page.js
+++ frontend/src/components/Page/Page.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import BiddingZoneList from "../BiddingZoneList";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const Page = (props) => {
   const { country } = useParams();
@@ -9,16 +10,38 @@ const Page = (props) => {
     (previous, zone) => (zone.country === country ? zone.country : previous),
     null
   );
+  let title = "European Electricity Prices";
+  if (countryName) {
+    title = props.title + countryName || "European Electricity Prices";
+  } else {
+    title = props.title || "European Electricity Prices";
+  }
 
   useEffect(() => {
-    if (countryName) {
-      document.title =
-        props.title + countryName || "European Electricity Prices";
-    } else {
-      document.title = props.title || "European Electricity Prices";
-    }
-  }, [props.title, countryName]);
-  return props.children;
+    document.title = title;
+  }, [title]);
+
+  return (
+    <>
+      <HelmetProvider>
+        <Helmet>
+          <meta property="og:url" content={window.location.href} />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={`Thingler - ${title}`} />
+          <meta property="og:image" content="https://thingler.io/map.png" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="thingler.io" />
+          <meta property="twitter:url" content={window.location.href} />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:description" content={`Thingler - ${title}`} />
+          <meta name="twitter:image" content="https://thingler.io/map.png" />
+        </Helmet>
+      </HelmetProvider>
+      {props.children}
+    </>
+  );
 };
 
 export default Page;
